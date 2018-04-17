f1b72a12c8fc82130ffcde18210d4fcc0349af7d
yadeniel <chasimpocity@gmail.com>
2018-04-17 15:26:42 +0700

    nambah index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+  <html>
+    <body>
+	    echo "Welcome To Mobile Legend";
+		echo "<br/>";
+		echo "Praktikum Teknologi Cloud Computing";
+		echo "Nim : 155610129";
+		echo "<br/>";
+		echo "Nama : Yadeniel Sakti B.A.";
+	</body>
+  </html>
+?>
